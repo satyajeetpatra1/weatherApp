c9dc4d47820cae3b24eb0146bf804999843ba9a1
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2025-10-08 16:13:56 +0530

    openweather api key and endpoints added!

---
+++ script.js
@@ -0,0 +1,7 @@
+// api key
+const OPENWEATHER_API_KEY = "043d60d85a7c98befa07698cc4ba1640";
+
+// api endpoints
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+const GEOCODE_URL = "https://api.openweathermap.org/geo/1.0/direct";
+const OPENWEATHER_ICON_URL = "https://openweathermap.org/img/wn";
